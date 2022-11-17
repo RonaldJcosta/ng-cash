ecb1e9d36ba7ac622c1dc7da47fec4675f3dc6b9
Ronald J Costa <ronald.jc@hotmail.com>
2022-11-17 17:32:25 -0300

    inicial project

---
+++ src/shared/http/server.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import router from './routes';
+import AppError from '@shared/errors/AppError';
+import { dataSource } from '../infra/typeorm/';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response
+        .status(error.statusCode)
+        .json({ status: 'error', message: error.message });
+    }
+
+    return response
+      .status(500)
+      .json({ status: 'error', message: `Internal Server Error` });
+  },
+);
+
+dataSource
+  .initialize()
+  .then(() => console.log(`DataSource foi inicializado.`))
+  .catch(err => console.error(`Houve um error no DataSource`, err));
+
+app.listen(3333, () => console.log(`Server is running http://localhost:3333`));
---
+++ src/shared/infra/typeorm/index.ts
@@ -0,0 +1,11 @@
+import { DataSource } from 'typeorm';
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'ngcash',
+  migrations: ['./src/shared/typeorm/migrations/*.ts'],
+});
